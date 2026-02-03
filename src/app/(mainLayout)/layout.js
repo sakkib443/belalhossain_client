038f0f766c10980691ab8cf0517e2eed6f36924f
sakibbdcallingit <mahabuburbdcalling@gmail.com>
2026-02-03 17:50:45 +0600

    Removed Admin Edit functionality (fixed button, provider and components)

--- src/app/(mainLayout)/layout.js
+++ src/app/(mainLayout)/layout.js
@@ -4,15 +4,12 @@ import Footer from '@/components/sheard/Footer';
 import Navbar from '@/components/sheard/Navbar';
 import TopHeader from '@/components/sheard/TopHeader';
 import Preloader from '@/components/sheard/Preloader';
-
-import AdminEditToggle from '@/components/shared/AdminEditToggle';
-import { AdminEditProvider } from '@/providers/AdminEditProvider';
 import React from 'react';
 import WhatsAppButton from '@/components/sheard/WhatsAppButton';
 
 const MainLayout = ({ children }) => {
     return (
-        <AdminEditProvider>
+        <>
             <Preloader />
             <div>
                 <TopHeader />
@@ -21,11 +18,9 @@ const MainLayout = ({ children }) => {
                 <Footer />
 
                 <WhatsAppButton />
-                <AdminEditToggle />
             </div>
-        </AdminEditProvider>
+        </>
     );
 };
 
 export default MainLayout;
-
